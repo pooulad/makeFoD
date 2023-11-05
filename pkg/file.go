68951e7a3fd35aac273d11e5dcc3c41de85556b8
pooulad <poooooladi@gmail.com>
2023-11-06 03:24:56 +0330

    Update file.go

--- pkg/file.go
+++ pkg/file.go
@@ -1,6 +1,23 @@
+package pkg
+
+import (
+	"fmt"
+	"os"
+)
+
 type MakeFoD struct {
 	dirName  string
 	fileName string
 	fnumber  int
 	dnumber  int
 }
+
+func NewMakeFoD(f *FlagReturns) *MakeFoD {
+	return &MakeFoD{
+		dirName:  f.DirName,
+		dnumber:  f.DNumber,
+		fnumber:  f.FNumber,
+		fileName: f.FileName,
+	}
+}
+
