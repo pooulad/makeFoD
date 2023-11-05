5fc046ef7cd3c01424c5ed7639135ea987185c4e
pooulad <poooooladi@gmail.com>
2023-11-05 20:24:41 +0330

    Update main.go

--- cmd/app/main.go
+++ cmd/app/main.go
@@ -1,7 +1,25 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"os"
+)
 
-func main(){
-	fmt.Println("hello go again...")
+func checkError(e error) {
+	if e != nil {
+		panic(e)
+	}
+
+}
+
+func main() {
+	count := 20
+	for i := 0; i < count; i++ {
+		f, err := os.Create(fmt.Sprintf("file%v.txt", i))
+		checkError(err)
+		fmt.Println("hello go again...")
+		fmt.Println(f)
+
+		defer f.Close()
+	}
 }
