f9673d5ca2b8fc900c35f70b7775268090a0b2dd
pooulad <poooooladi@gmail.com>
2023-11-06 03:25:12 +0330

    Update read_flag.go

--- pkg/read_flag.go
+++ pkg/read_flag.go
@@ -12,3 +12,17 @@ type FlagReturns struct {
 	FNumber  int
 }
 
+func ReadFlag() (*FlagReturns, error) {
+	fr := FlagReturns{}
+	flag.StringVar(&fr.FileName, "f", "", "get file name")
+	flag.StringVar(&fr.DirName, "d", "", "get directory name")
+	flag.IntVar(&fr.DNumber, "dn", 0, "get number of directories for create")
+	flag.IntVar(&fr.FNumber, "fn", 0, "get number of files for create")
+	flag.Parse()
+
+	if fr.FileName == "" {
+		return nil, errors.New("please enter a valid file address")
+	}
+
+	return &fr, nil
+}
