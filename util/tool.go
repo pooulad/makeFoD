b46f225d03d7b32d324ab45bed9fd2560e41860c
pooulad <poooooladi@gmail.com>
2023-11-06 03:39:24 +0330

    Update tool.go

--- util/tool.go
+++ util/tool.go
@@ -1,6 +1,14 @@
 package util
 
-func CheckError(e error) {
+import "log"
+
+func FatalError(e error) {
+	if e != nil {
+		log.Fatal(e)
+	}
+}
+
+func PanicError(e error) {
 	if e != nil {
 		panic(e)
 	}
